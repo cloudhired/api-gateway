b1be0ef9217d23cdc6d0720a935471bb4ec6403c
gaomengen <gaomengen@gmail.com>
2020-12-21 00:23:31 -0500

    udpated region

--- deploy_run.sh
+++ deploy_run.sh
@@ -5,5 +5,6 @@ gcloud run deploy gateway \
   --image="$(cat /workspace/new_image.txt)" \
   --set-env-vars=ESPv2_ARGS=--cors_preset=basic \
   --allow-unauthenticated \
+  --region=us-central1
   --platform managed \
   --project=cloudhired
