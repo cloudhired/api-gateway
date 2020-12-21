e6294006ffb6064029edb2c43dd092c738ee2541
gaomengen <gaomengen@gmail.com>
2020-12-21 00:25:33 -0500

    udpated platform

--- deploy_run.sh
+++ deploy_run.sh
@@ -6,5 +6,5 @@ gcloud run deploy gateway \
   --set-env-vars=ESPv2_ARGS=--cors_preset=basic \
   --allow-unauthenticated \
   --region=us-central1
-  --platform managed \
+  --platform=managed \
   --project=cloudhired
