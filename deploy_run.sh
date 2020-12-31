ddce00347b627ed4d50c10079c5c808620142f65
gaomengen <gaomengen@gmail.com>
2020-12-31 16:45:05 -0500

    can we get this over? 1

--- deploy_run.sh
+++ deploy_run.sh
@@ -3,7 +3,7 @@
 # ESPv2_ARGS delimiter defined by ^^ e.g. ^++^ delimiter is ++
 gcloud run deploy gateway \
   --image="$(cat /workspace/new_image.txt)" \
-  --set-env-vars=ESPv2_ARGS=^++^--cors_preset=basic++--cors_allow_headers="x-auth-token" \
+  --set-env-vars=ESPv2_ARGS=^++^--cors_preset=basic++--cors_allow_headers='Authorization,x-auth-token' \
   --allow-unauthenticated \
   --region=us-central1 \
   --platform=managed \
