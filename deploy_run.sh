b793c1c2d7bdedf4424a2fd297d81e06a7ec53d7
gaomengen <gaomengen@gmail.com>
2020-12-31 15:37:25 -0500

    escape

--- deploy_run.sh
+++ deploy_run.sh
@@ -4,7 +4,7 @@
 # ESPv2_ARGS delimiter defined by ^^ e.g. ^++^ delimiter is ++
 gcloud run deploy gateway \
   --image="$(cat /workspace/new_image.txt)" \
-  --set-env-vars=ESPv2_ARGS=\^++\^--cors_preset=basic++--cors_allow_headers=DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Authorization,x-auth-token \
+  --set-env-vars=ESPv2_ARGS=^^^^++^^^^--cors_preset=basic++--cors_allow_headers=DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Authorization,x-auth-token \
   --allow-unauthenticated \
   --region=us-central1 \
   --platform=managed \
