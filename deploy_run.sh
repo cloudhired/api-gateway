bdd076d2adb9227a6ddfb3541f3b2e888f1f0869
gaomengen <gaomengen@gmail.com>
2020-12-31 15:21:13 -0500

    cors config 1

--- deploy_run.sh
+++ deploy_run.sh
@@ -5,7 +5,7 @@
 # no quotes for any of the cors params
 gcloud run deploy gateway \
   --image="$(cat /workspace/new_image.txt)" \
-  --set-env-vars=ESPv2_ARGS=^++^--cors_preset=basic++--cors_allow_headers=DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Authorization,x-auth-token \
+  --set-env-vars=ESPv2_ARGS=^++^--cors_preset=basic++--cors_allow_headers="DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Authorization,x-auth-token" \
   --allow-unauthenticated \
   --region=us-central1 \
   --platform=managed \
