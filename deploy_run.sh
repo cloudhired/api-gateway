04bb49f162cfb6f6b2ffca4610635a2cccfb1bad
gaomengen <gaomengen@gmail.com>
2020-12-31 14:39:37 -0500

    testing cors config

--- deploy_run.sh
+++ deploy_run.sh
@@ -3,7 +3,7 @@
 
 gcloud run deploy gateway \
   --image="$(cat /workspace/new_image.txt)" \
-  --set-env-vars=ESPv2_ARGS=--cors_preset=basic \
+  --set-env-vars=ESPv2_ARGS=^++^--cors_preset=basic++--cors_allow_origin=your_host.com \
   --allow-unauthenticated \
   --region=us-central1 \
   --platform=managed \
