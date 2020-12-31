c5441e6b087d9bfca10481d752ada783722d910e
gaomengen <gaomengen@gmail.com>
2020-12-31 16:34:10 -0500

    maybe it is not because of carets

--- deploy_run.sh
+++ deploy_run.sh
@@ -3,7 +3,7 @@
 # ESPv2_ARGS delimiter defined by ^^ e.g. ^++^ delimiter is ++
 gcloud run deploy gateway \
   --image="$(cat /workspace/new_image.txt)" \
-  --set-env-vars=ESPv2_ARGS=--cors_preset=basic \
+  --set-env-vars=ESPv2_ARGS=^++^--cors_preset=basic++--cors_allow_credentials \
   --allow-unauthenticated \
   --region=us-central1 \
   --platform=managed \
